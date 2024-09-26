06326e1cd7f6a85c26c73c79f773a40d7f5626c5
KARA NORTON <nortonkara057@gmail.com>
2024-09-26 16:10:21 -0400

    fetch fake products

--- src/app/views/ProductsView.tsx
+++ src/app/views/ProductsView.tsx
@@ -1,5 +1,35 @@
-const ProductsView = () => {
-    return <div>Products</div>
-}
+import React, { useState, useEffect } from 'react';
 
-export default ProductsView
+const ProductsView = () => {
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=5')
+            .then((res) => res.json())
+            .then((json) => setProducts(json));
+    }, []);
+
+    return (
+        <>
+            <div>Products</div>
+            <div>
+                <table>
+                    <thead>
+                        <td key={2} id={'2'}>
+                            table header
+                        </td>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td key={1} id={'1'}>
+                                table data
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+};
+
+export default ProductsView;
