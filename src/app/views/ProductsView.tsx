7309577b72d6fc293a1a3b12d1243017eb21e534
KARA NORTON <nortonkara057@gmail.com>
2024-09-26 21:57:28 -0400

    sort product columns on click

--- src/app/views/ProductsView.tsx
+++ src/app/views/ProductsView.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getProducts } from '../api/products';
+import { Products } from '../types/Products';
 
 const ProductsView = () => {
+    // the product data is returned in asc order by id
+    const [asc, setAsc] = useState<boolean>(false);
+    const [sortBy, setSortBy] = useState<string>('id');
     const products = getProducts();
 
+    const sortColumn = (prop: keyof Products) => {
+        if (asc || sortBy !== prop) {
+            products.sort((a, b) => {
+                if (prop === 'rating') {
+                    return +a.rating.rate - +b.rating.rate;
+                } else if (typeof a[prop] === 'number') {
+                    return +a[prop] - +b[prop];
+                } else {
+                    const aUpper = a[prop].toString().toUpperCase();
+                    const bUpper = b[prop].toString().toUpperCase();
+                    if (aUpper > bUpper) {
+                        return 1;
+                    } else if (aUpper < bUpper) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        } else {
+            products.sort((a, b) => {
+                if (prop === 'rating') {
+                    return +b.rating.rate - +a.rating.rate;
+                } else if (typeof a[prop] === 'number') {
+                    return +b[prop] - +a[prop];
+                } else {
+                    const aUpper = a[prop].toString().toUpperCase();
+                    const bUpper = b[prop].toString().toUpperCase();
+                    if (aUpper > bUpper) {
+                        return -1;
+                    } else if (aUpper < bUpper) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+        setSortBy(prop);
+        (sortBy === prop || asc) && setAsc(!asc);
+    };
     return (
         <>
             <div>Products</div>
@@ -13,7 +58,14 @@ const ProductsView = () => {
                         <tr>
                             {products?.length > 0 &&
                                 Object.keys(products[0]).map((prop) => (
-                                    <th key={prop}>{prop}</th>
+                                    <th
+                                        key={prop}
+                                        onClick={(e) =>
+                                            sortColumn(prop as keyof Products)
+                                        }
+                                    >
+                                        {prop}
+                                    </th>
                                 ))}
                         </tr>
                     </thead>
